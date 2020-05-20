cd43df7493a99a4a0f7adb4079109559d3043640
Marco Rebsamen <mrebsamen@unimatrix0.ch>
2020-05-20 14:44:54 +0200

    Initial Commit mit FontAwesomeIcons

---
+++ Mobile/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Mobile.Services;
+using Mobile.Views;
+
+namespace Mobile
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+
+        private void InitializeIcons()
+        {
+            Plugin.Iconize.Iconize
+                .With(new Plugin.Iconize.Fonts.FontAwesomeRegularModule())
+                .With(new Plugin.Iconize.Fonts.FontAwesomeBrandsModule())
+                .With(new Plugin.Iconize.Fonts.FontAwesomeSolidModule());            
+        }
+    }
+}
