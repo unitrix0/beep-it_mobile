2dfeb221fdf77c32488595bab19f8c0079c0339e
Marco Rebsamen <mrebsamen@unimatrix0.ch>
2020-06-07 20:27:43 +0200

    Tabbed Pages und Icons

---
+++ Mobile/Views/ShoppingListPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShoppingListPage : ContentPage
+    {
+        public ShoppingListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
